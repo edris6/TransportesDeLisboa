f831022057dfb033d74a60afc9bc7e4b6bfc7268
edris <soccerskin87@gmail.com>
2025-01-06 19:41:49 +0000

    testing out comboios api

--- src/comboios.mjs
+++ src/comboios.mjs
@@ -1,17 +1,19 @@
-import comboios from 'comboios';
+import comboios from "comboios";
 
-
-async function station_exists(name){
-    let available_stations = await comboios.stations()
-    for(let i = 0;i < available_stations.length; i++){
-        if (available_stations[i].station == name){
-            return true
-        }
+async function station_exists(name) {
+  let available_stations = await comboios.stations();
+  console.log(available_stations);
+  for (let i = 0; i < available_stations.length; i++) {
+    if (available_stations[i].name == name) {
+      return [true, available_stations[i].id];
     }
-    return false
+  }
+  return [false, null];
 }
-  
+
 (async () => {
-    const exists = await station_exists("123");
-    console.log(exists); 
+  const exists = await station_exists("Santos");
+  if (exists[0]) {
+    console.log(await comboios.stopovers(exists[1]));
+  }
 })();
